4ae2d958dbcc83ca35c678f04c2f3f04f687eb44
Mathieu D <mathieu@dartic.fr>
2017-10-17 22:02:30 +0200

    Make it work

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
   ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+      ],
+    }],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
