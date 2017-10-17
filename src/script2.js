7f59be58c6ca74b61389e429c9f3dea3226f17d3
Mathieu D <mathieu@dartic.fr>
2017-10-17 21:52:30 +0200

    Enable HMR and fix script2

--- src/script2.js
+++ src/script2.js
@@ -1,6 +1,6 @@
 /* eslint no-console: ["error", { allow: ["log"] }] */
-/* global window c */
-
+/* global window */
+const c = 'pouet';
 console.log(`Hello World ${window.a}`);
 console.log(`Hello World ${window.b}`);
 console.log(`Hello World ${window.c}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   devtool: 'source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
