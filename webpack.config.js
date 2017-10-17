72f2631b5406cc1c7c8599fb2805db1d24300146
Mathieu D <mathieu@dartic.fr>
2017-10-17 22:13:59 +0200

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   devtool: 'source-map',
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Example Project for JavaScript training',
+    }),
     new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
