299dec16e09a3664ba89bf15ad48df8ce774361b
Mathieu D <mathieu@dartic.fr>
2017-10-17 21:04:09 +0200

    Fix lint issues

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var a = 'b';
-let b = 'c';
+/* eslint no-console: ["error", { allow: ["log"] }] */
+/* global window */
+
+const a = 'b';
+const b = 'c';
 const c = 'constant';
-console.log('Hello World ' + a + b + c);
-console.log('Hello World ' + window.a + window.b + window.c);
+console.log(`Hello World ${a} ${b} ${c}`);
+console.log(`Hello World ${window.a}${window.b}${window.c}`);
--- script2.js
+++ script2.js
@@ -1,6 +1,9 @@
-console.log('Hello World ' + window.a);
-console.log('Hello World ' + window.b);
-console.log('Hello World ' + window.c);
-console.log('Hello World ' + c);
-var a = 'c';
-console.log('Hello World ' + a);
+/* eslint no-console: ["error", { allow: ["log"] }] */
+/* global window c */
+
+console.log(`Hello World ${window.a}`);
+console.log(`Hello World ${window.b}`);
+console.log(`Hello World ${window.c}`);
+console.log(`Hello World ${c}`);
+const a = 'c';
+console.log(`Hello World ${a}`);
