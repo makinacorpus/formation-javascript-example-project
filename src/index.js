f6348b19d289aedfdb8f6f802639b5b581d467b2
Mathieu D <mathieu@dartic.fr>
2017-10-17 23:04:42 +0200

    Update index js with destructuring & spread operator

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ const b = 'c';
 const c = 'constant';
 console.log(`Hello World ${a} ${b} ${c}`);
 console.log(`Hello World ${window.a}${window.b}${window.c}`);
+
+const myArray = ['pouet', 'pouic'];
+const myArray2 = [...myArray, 'pouet2'];
+const myObject = { destination: 'Paris', origine: 'Toulouse' };
+const { destination, origine } = myObject;
+
+console.log(destination, origine, myObject);
+console.log(myArray2, myArray);
+
