b69298389eb13820fe80fa72d378ebb6174c88a3
Mathieu D <mathieu@dartic.fr>
2017-10-17 21:43:58 +0200

    Add devtool conf

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'sourcemap',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
